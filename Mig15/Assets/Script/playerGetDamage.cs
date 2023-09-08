eb356b39ffad2a940f912d3e829ec4243f151980
Rgrach <32617088+RomaGrach@users.noreply.github.com>
2023-09-09 00:34:47 +0300

    добавил индикатор здоровья игрока

--- Mig15/Assets/Script/playerGetDamage.cs
+++ Mig15/Assets/Script/playerGetDamage.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerGetDamage : MonoBehaviour
 {
     [SerializeField] GameObject _bricksEffectPrefab;
-    public int hp = 3;
+    public float hp;
+    public float Maxhp = 3f;
+    Image healthBar;
+    [SerializeField] GameObject Image;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = Maxhp;
+        healthBar = Image.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        healthBar.fillAmount = hp / Maxhp;
         if (hp <= 0)
         {
             FindObjectOfType<GameManager>().ShowFinishWindowBadEnd();
