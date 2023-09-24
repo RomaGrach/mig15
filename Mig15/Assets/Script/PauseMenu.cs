60235dcf22d1beea1151e49d48725aee42951ac8
SBC-RU <114054851+SBC-RU@users.noreply.github.com>
2023-09-24 17:57:27 +0300

    Добавил подсказки игроку

--- Mig15/Assets/Script/PauseMenu.cs
+++ Mig15/Assets/Script/PauseMenu.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public Comments[] tips;
+    public Text comments_txt;
+    //public Text comments2;
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
     // Update is called once per frame
@@ -31,8 +35,18 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
+        CommentsAct();
         Time.timeScale=0f;
         GameIsPaused = true;
+    }
+    void CommentsAct()
+    {
+        int i = Random.Range(0, tips.Length);
+        comments_txt.text = tips[i].Ru;
+        //comments_txt.text = tips[1].Ru;
+        //comments_txt.text = tips[2].Ru;
+        //comments2.text = tips[1].Ru;
+
     }
     public void Button_exit()
     {
